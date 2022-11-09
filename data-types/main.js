c45236548f30aaa742c07cd0fdd6be0e960bb10d
ZhDev0 <zhdevz00@gmail.com>
2022-11-09 23:05:05 +0100

    First commit

---
+++ data-types/main.js
@@ -0,0 +1,14 @@
+/*
+Data Types Intro
+    - String
+    - Number
+    - Array => Object
+*/
+console.log("Omar Zairh");
+
+console.log(typeof "Omar Zairh");
+console.log(typeof "5000");
+console.log(typeof 5000);
+console.log(typeof 5000.99);
+console.log(typeof [10, 15, 17]);
+console.log(typeof ["Omar", "Imran", "Chaimae"]);
