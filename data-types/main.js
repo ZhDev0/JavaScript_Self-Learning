ad6037be9b119273c0154bb234197f3b9a6152cb
ZhDev0 <zhdevz00@gmail.com>
2022-11-09 23:11:39 +0100

    second commit

--- data-types/main.js
+++ data-types/main.js
@@ -3,6 +3,8 @@ Data Types Intro
     - String
     - Number
     - Array => Object
+    - Object
+    - Boolean
 */
 console.log("Omar Zairh");
 
@@ -12,3 +14,8 @@ console.log(typeof 5000);
 console.log(typeof 5000.99);
 console.log(typeof [10, 15, 17]);
 console.log(typeof ["Omar", "Imran", "Chaimae"]);
+console.log(typeof {name: 'Omar', age: 22, country: 'Morocco'});
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
