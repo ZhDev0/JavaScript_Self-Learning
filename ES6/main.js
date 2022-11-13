0b8a93ee05484e8911d68655833a94988c82d5cf
ZhDev0 <zhdevz00@gmail.com>
2022-11-13 02:05:42 +0100

    ES6

---
+++ ES6/main.js
@@ -0,0 +1,20 @@
+/*
+ES6
+is an international association dedicated to the standardization of information and communication systems
+these information can be a programming language
+so ES6 gives a programming language a standard to go with 
+
+visit ES6 Features Web Site
+
+
+not all browser support ES6 but all modern browser supports ES6
+
+Visit Babel // we write a modern js code and Babel convert it into legacy code
+*/
+
+
+var myName = "Omar"
+
+console.log('Hello ' + " " + myName)
+//ES6
+console.log(`Hello ${myName}`)
